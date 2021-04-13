f31001d001a946d83f76763cf3457dfb87898774
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2021-04-12 22:55:06 -0300

    refactor(header): change Link to ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
-import styles from './styles.module.scss';
-
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
+
+import styles from './styles.module.scss';
 
 export function Header() {
   return (
@@ -8,8 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
